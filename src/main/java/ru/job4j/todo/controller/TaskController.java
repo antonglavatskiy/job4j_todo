11c8f7894278e8f76144ed5afddf39899dc342ee
antonglavatskiy <glavazky@yandex.ru>
2023-06-14 21:02:36 +0500

    6. Добавлена форма создания задания и кнопка в списке заданий

--- src/main/java/ru/job4j/todo/controller/TaskController.java
+++ src/main/java/ru/job4j/todo/controller/TaskController.java
@@ -3,7 +3,10 @@ package ru.job4j.todo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.job4j.todo.model.Task;
 import ru.job4j.todo.service.TaskService;
 
 @Controller
@@ -33,4 +36,16 @@ public class TaskController {
         model.addAttribute("tasks", taskService.findAllNew());
         return "tasks/list";
     }
+
+    @GetMapping("/create")
+    public String getCreationPage() {
+        return "tasks/create";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute Task task, Model model) {
+        taskService.save(task);
+        return "redirect:/tasks";
+    }
+
 }
